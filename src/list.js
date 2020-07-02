8908510587cc3ed9b2fae2842683d8fb0f51d5e5
Bogdan Sutuzhko <bogdan.sutuzhko.19@gmail.com>
2020-07-02 18:08:38 +0300

    Слайдер баг решен

--- src/list.js
+++ src/list.js
@@ -3,6 +3,6 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./list.pug");
 }
 
-// import "./scripts/burger-menu";
-// import "./scripts/slider";
-// import "./scripts/search";
+import "./scripts/burger-menu";
+import "./scripts/slider";
+import "./scripts/search";
